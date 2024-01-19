5c47738dd904ca94fa3e5b8df25f8ba331c4e894
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-19 14:34:36 +0700

    Add datastructure files for LinkedListQueue

---
+++ src/datastructure/Node.java
@@ -0,0 +1,16 @@
+package datastructure;
+
+public class Node {
+    public Object info;
+    public Node next;
+
+    public Node(Object x, Node p) {
+        info = x;
+        next = p;
+    }
+
+    public Node(Object x) {
+        this(x, null);
+    }
+
+}
